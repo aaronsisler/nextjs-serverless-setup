70fb478f8439f44f7fe4d70be1353bbc91d51361
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2019-05-09 15:49:33 -0400

    Changed some stuff, but committing to begin slimming this down to simple. I want it to work first

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 // next.config.js
-const path = require('path');
-const withSass = require('@zeit/next-sass');
-require('dotenv').config({ path: path.resolve(__dirname, `./env/${process.env.ENV_FILE}`) });
+const path = require("path");
+const withSass = require("@zeit/next-sass");
+require("dotenv").config({
+  path: path.resolve(__dirname, `./env/${process.env.ENV_FILE}`)
+});
 
 const { STATIC_PATH } = process.env;
 
 module.exports = Object.assign(
   {},
-  withSass(),
-  {
+  withSass({
     publicRuntimeConfig: {
-      STATIC_PATH,
-    },
-  }
+      STATIC_PATH
+    }
+  })
 );
---
+++ pages/contact.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../static/scss/main.scss";
+
+export default () => (
+  <div className="contact">
+    <h1>Contact</h1>
+  </div>
+);
